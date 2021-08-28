0f46e2ad607c135250da9c752a86657c58722be2
Aishwarya-hub-create <85681067+Aishwarya-hub-create@users.noreply.github.com>
2021-08-28 10:54:17 +0530

    Update C81main.js

--- C81main.js
+++ C81main.js
@@ -6,7 +6,7 @@ color="red";
 ctx.beginPath();
 ctx.strokeStyle=color;
 ctx.lineWidth=2;
-ctx.arc(200,200,100,0,2*Math.P1);
+ctx.arc(200,200,100,0,2*Math.Pi);
 ctx.stroke();
 
 canvas.addEventListener("mousedown",my_mousedown);
